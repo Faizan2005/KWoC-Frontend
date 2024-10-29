729ffe8b61635f9e4ce2acdce026371afa69bd80
Bikram Ghuku <bikramghuku05@gmail.com>
2024-10-29 19:37:27 +0530

    feat: add pagination based selection

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,9 +3,9 @@ import { Project } from "../types"
 function ListItem({ item } : {item : Project}) {
     return (
         <div className="ls-comp">
-            <div className="ls-comp-name">{item.Name}</div>
-            <div className="ls-comp-mentor">{item.men_a_name}</div>
-            <div className="ls-comp-pull">{item.pulls}</div>
+            <div className="ls-comp-name">{item.name}</div>
+            <div className="ls-comp-mentor">{item.mentorAName}</div>
+            <div className="ls-comp-pull">{item.pullRequests}</div>
         </div>
     )
 }
--- src/pages/OrgDash.tsx
+++ src/pages/OrgDash.tsx
@@ -1,24 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ListItem from '../components/ListItem'
 import DetailsViewer from '../components/DetailsViewer'
 import { Project } from '../types'
+import testData from '../testData.json'
+import { PAGENATION_LEN } from '../constants'
 
 function OrgDash() {
 	const [allData, setAllData] = useState<Project[]>([]);
 	const [filterData, setFilterData] = useState<Project[]>([]);
+	const [currPage, setCurrPage] = useState<Project[]>([]);
+	const [pgNo, setPgNo] = useState<number>(1);
 
 	const [selItem, setSelItem] = useState<Project>({} as Project);
+
+	useEffect(() => {
+		setAllData(testData as Project[]);
+		setFilterData(allData);
+	}, []);
+
+	useEffect(() => {
+		const startidx = (pgNo - 1) * PAGENATION_LEN;
+		const endidx = Math.min(startidx + PAGENATION_LEN, filterData.length);
+		setCurrPage(filterData.slice(startidx, endidx));
+	}, [pgNo]);
 
 	const showAll = () => {
 		setFilterData(allData);
 	}
 
 	const showAccept = () => {
-		setFilterData(allData.filter((item) => item.ProjectStatus))
+		setFilterData(allData.filter((item) => item.isProjectActive))
 	}
 
 	const showReject = () => {
-		setFilterData(allData.filter((item) => !item.ProjectStatus))
+		setFilterData(allData.filter((item) => !item.isProjectActive))
+	}
+
+	const nextPage = () => {
+		if(pgNo <= filterData.length / PAGENATION_LEN) setPgNo(pgNo + 1);
+	}
+
+	const prevPage = () => {
+		if(pgNo >= 2) setPgNo(pgNo - 1);
 	}
 	return (
 		<div>
@@ -35,7 +58,21 @@ function OrgDash() {
 					</div>
 				</div>
 				<div className="org-dash-view">
-					
+					<div className="org-dash-table">
+						{currPage.map((value, idx) => (
+							<div>
+								<ListItem item={value}/>
+								{idx + 1}
+							</div>
+						))}
+					</div>
+					<div className="org-dash-table-nav">
+						<button className='org-dash-table-back' onClick={() => prevPage()}>&lt;</button>
+						<div className="org-dash-table-info">
+							Showing {(pgNo - 1) * PAGENATION_LEN + 1} - {pgNo * PAGENATION_LEN} of {filterData.length}
+						</div>
+						<button className='org-dash-table-next' onClick={() => nextPage()}>&gt;</button>
+					</div>
 				</div>
 			</div>
 			<div className="org-dash-right">
