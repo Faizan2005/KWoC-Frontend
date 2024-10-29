ccf95125f1b18e4c9053c2e9e330f82d6b36186d
prem12937@gmail.com <prem12937@gmail.com>
2024-10-30 01:02:57 +0530

    style: add basic navbar design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,10 @@
 import "../styles/Header.css";
-import { ROUTER_PATHS } from "../util/constants";
-import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+
 export const Header = () => {
   return (
     <div className="header-container">
-      <div className="kwoc-logo"></div>
-      <nav className="nav-bar">
-        <a href='#' className="nav-element">Home</a>
-        <a href='#' className="nav-element">Projects</a>
-        <a href='#' className="nav-element">FAQs</a>
-        <button className="button"><Link to = {ROUTER_PATHS.STUDENT_FORM}>Register</Link></button>
-      </nav>
+      <Navbar />
     </div>
   )
 }
