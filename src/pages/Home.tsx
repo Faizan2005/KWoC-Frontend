f75423ab6d6e6c6874299fb8c536d6c128ea8d99
Tegan Jain <teganjain527@gmail.com>
2024-11-04 23:34:40 +0530

    snow: make snowflakes better

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,14 +9,15 @@ function Home() {
   return (
     <>
       <Snowfall // the image can be edited into anything we want, feel free to change
-        snowflakeCount={200}
+        snowflakeCount={150}
         speed={[0.5, 1]} // array takes [min, max]
         wind={[-0.5, 0.5]}
-        radius={[1, 2]}
+        radius={[2, 4]}
         style={{
           position: 'fixed',
           width: '100vw',
           height: '100vh',
+          opacity: '0.5',
         }}
       />
       <Header />
